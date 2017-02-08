42ba100dd7c73605c34b46997ac6c76e0b706497
Klonno <scott.teerink@gmail.com>
2017-02-08 13:58:20 -0800

    Add service and blinking thread.

--- WinformWCF/Form1.Designer.cs
+++ WinformWCF/Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.blinkButton = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.callServiceButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // blinkButton
+            // 
+            this.blinkButton.AutoSize = true;
+            this.blinkButton.Location = new System.Drawing.Point(215, 24);
+            this.blinkButton.Name = "blinkButton";
+            this.blinkButton.Size = new System.Drawing.Size(87, 17);
+            this.blinkButton.TabIndex = 0;
+            this.blinkButton.TabStop = true;
+            this.blinkButton.Text = "I should blink";
+            this.blinkButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Stop Blinking";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // callServiceButton
+            // 
+            this.callServiceButton.Location = new System.Drawing.Point(12, 206);
+            this.callServiceButton.Name = "callServiceButton";
+            this.callServiceButton.Size = new System.Drawing.Size(174, 23);
+            this.callServiceButton.TabIndex = 2;
+            this.callServiceButton.Text = "Call Test WCF Service";
+            this.callServiceButton.UseVisualStyleBackColor = true;
+            this.callServiceButton.Click += new System.EventHandler(this.callServiceButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 45);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "An instance of IIS express  will start (if not already running) supporting  the W" +
+    "CFService API at http://localhost:16371/. The URL is specified as an endpoint in" +
+    " the app.config file.\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.callServiceButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.blinkButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton blinkButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button callServiceButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WinformWCF/Form1.cs
+++ WinformWCF/Form1.cs
@@ -5,16 +5,84 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinformWCF
 {
+
+
     public partial class Form1 : Form
     {
+        CancellationTokenSource _tokenSource = new CancellationTokenSource();
+        CancellationToken _cancelToken;
+        private delegate void SetCheckDelegate(RadioButton button, bool set);
+
+
         public Form1()
         {
             InitializeComponent();
+            _cancelToken = _tokenSource.Token;
+            Action blinkMethod = new Action(() => BlinkTheButton(this));
+            Task blinker = new Task(blinkMethod, _cancelToken);
+            blinker.Start();
+        }
+
+        private void BlinkTheButton(Form1 form)
+        {
+            bool state = true;
+
+            while (WaitHandle.WaitAll(new WaitHandle[] { _cancelToken.WaitHandle }, 1000) == false)
+            {
+                if (state == true)
+                {
+                    state = false;
+                }
+                else
+                {
+                    state = true;
+                }
+                SetBlinkButton(state);
+            }
+        }
+
+        private void ThreadSafeSetCheck(RadioButton button, bool set)
+        {
+            button.Checked = set;
+        }
+
+        private void SetBlinkButton(bool check)
+        {
+            if (this.blinkButton.InvokeRequired)
+            {
+                SetCheckDelegate d = new SetCheckDelegate(ThreadSafeSetCheck);
+                this.blinkButton.Invoke(d, new object[] { blinkButton, check });
+            }
+            else
+            {
+                this.blinkButton.Checked = check;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _tokenSource.Cancel();
+        }
+
+        private void callServiceButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WinformWCF.TestWCFServiceReference.TestWCFServiceClient serviceClient = new TestWCFServiceReference.TestWCFServiceClient();
+
+                string str = serviceClient.GetData(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
